88302c3a7d39b15bc03c255590c3d4c7dd36762c
User <mikhailbogdan2006@gmail.com>
2026-03-24 13:51:08 +0300

    Data caching

--- src/main/java/com/example/messenger/repository/MessageRepository.java
+++ src/main/java/com/example/messenger/repository/MessageRepository.java
@@ -17,14 +17,13 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
 
     List<Message> findByChatId(Long chatId);
 
-    // 1. Сложный запрос через JPQL (фильтрация по вложенной сущности Chat)
     @Query("SELECT m FROM Message m JOIN m.chat c WHERE c.title LIKE %:chatTitle% AND m.content LIKE %:keyword%")
     Page<Message> searchByChatAndContentJpql(
             @Param("chatTitle") String chatTitle,
             @Param("keyword") String keyword,
             Pageable pageable);
 
-    // 2. Аналогичный запрос через Native Query (чистый SQL)
+
     @Query(value = "SELECT m.* FROM messages m INNER JOIN chats c ON m.chat_id = c.id WHERE c.title ILIKE %:chatTitle% AND m.content ILIKE %:keyword%",
             countQuery = "SELECT count(*) FROM messages m INNER JOIN chats c ON m.chat_id = c.id WHERE c.title ILIKE %:chatTitle% AND m.content ILIKE %:keyword%",
             nativeQuery = true)
